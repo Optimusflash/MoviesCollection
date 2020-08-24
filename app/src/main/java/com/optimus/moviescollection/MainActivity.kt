36b0d86d4dde7e1a4232cd7d0c2c348e171a8201
Dmitriy Chebotar <optimussflash@ukr.net>
2020-08-24 19:03:58 +0300

    enable firebase analytics, crashlytics

--- app/src/main/java/com/optimus/moviescollection/MainActivity.kt
+++ app/src/main/java/com/optimus/moviescollection/MainActivity.kt
@@ -2,6 +2,7 @@ package com.optimus.moviescollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
